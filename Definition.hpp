47b5ac3f20eee767726d3091bbe9cccf40db3d8b
Jin3004 <jinjin30004@gmail.com>
2019-10-01 20:06:46 +0900

    Update(19/10/1)

--- Definition.hpp
+++ Definition.hpp
@@ -2,6 +2,8 @@
 #include <Siv3D.hpp>
 #include <memory>
 #include <thread>
+#include <vector>
+#include <fstream>
 //Include dependent files.
 
 extern constexpr int WINDOW_X = 1280;
@@ -11,9 +13,11 @@ extern constexpr int WINDOW_Y = 720;
 class Game {
 public:
 	Game() = default;
-	Font debug{ 20 };
+
+	Font debug_{ 20, U"Noto Sans CJK JP Medium" };
+
 	virtual void Initialize(bool& isloading) {};
-	virtual std::unique_ptr<Game> Calculation(bool& isloading) { return nullptr; };
+	[[nodiscard]] virtual std::unique_ptr<Game> Calculation(bool& isloading) { return nullptr; };
 	virtual void Draw(bool isloading)const {};
 	virtual void End() {};
 };
@@ -21,9 +25,28 @@ public:
 
 class Start :public Game {
 public:
+
+	Texture title_{ U"resource/img/title.png" };
+
 	Start() = default;
 	void Initialize(bool& isloading)override;
-	std::unique_ptr<Game> Calculation(bool& isloading)override;
+	[[nodiscard]] std::unique_ptr<Game> Calculation(bool& isloading)override;
 	void Draw(bool isloading)const override;
 	void End()override;
+};
+
+template<class Func>
+class Menu :public Game {
+public:
+
+	std::vector<String> labels;
+	std::vector<String> buttons;
+	std::vector<String> texture_name;
+
+	Menu() = default;
+	void Initialize(bool& isloading)override;
+	Menu(std::initializer_list<String> l, std::initializer_list<String> b) :labels(l), buttons(b) {};
+	[[nodiscard]] std::unique_ptr<Game> Calculation(bool& isloading)override;
+	void Draw(bool isloading)const override;
+	//void End()override;
 };
--- Main.cpp
+++ Main.cpp
@@ -3,14 +3,16 @@
 void Main() {//Entry point
 
 	bool state = true, isloading = false;
+	Rect black_screen(0, 0, WINDOW_X, WINDOW_Y);
 	std::unique_ptr<Game> game_ptr = std::make_unique<Start>();
-	
+
 	std::thread calc([&]() {
 		game_ptr->Initialize(isloading);
 
 		while (state) {
 			auto ischanged = game_ptr->Calculation(isloading);
 			if (ischanged != nullptr) {
+				game_ptr->End();
 				game_ptr = std::move(ischanged);
 				game_ptr->Initialize(isloading);
 			}
@@ -18,8 +20,15 @@ void Main() {//Entry point
 		});
 
 	std::thread draw([&]() {
+
+		Font debug{ 20 };
+
 		while (state = System::Update()) {
 			game_ptr->Draw(isloading);
+			if (isloading) {
+				black_screen.draw(Palette::Black);
+				debug(U"Now Loading...").draw(0, 0);
+			}
 		}
 		});
 
@@ -35,15 +44,25 @@ void Start::Initialize(bool& isloading) {
 	isloading = false;
 }
 
-std::unique_ptr<Game> Start::Calculation(bool& isloading) {
-	return nullptr;
+[[nodiscard]] std::unique_ptr<Game> Start::Calculation(bool& isloading) {
+	if (!KeySpace.down())return nullptr;
+	else return std::make_unique<Menu>();
 }
 
 void Start::Draw(bool isloading) const {
-	if (isloading)debug(U"Now Loading...").draw(0, 0);
-	else debug(U"Finish loading.").draw(0, 0);
+	title_.draw();
 }
 
 void Start::End() {
 
+}
+
+void Menu::Initialize(bool& isloading) {
+}
+
+[[nodiscard]] std::unique_ptr<Game> Menu::Calculation(bool& isloading) {
+	return nullptr;
+}
+
+void Menu::Draw(bool isloading)const {
 }
